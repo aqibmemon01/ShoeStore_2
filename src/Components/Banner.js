12814a6e00243da67ef56c40d009de708eae07a5
aqibmemon01 <maqibmemon786@gmail.com>
2021-03-09 11:03:23 +0500

    Initial Commit

---
+++ src/Components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+const Banner = () => {
+  return(
+   
+    <header>
+        <div className="headerText" >
+          <div>
+          <h1>Welcome to Memon Garments</h1>
+          <p>Enjoy Online Shopping with Us! </p>
+          </div>
+        </div>
+    </header>
+   
+  )
+}
+export default Banner; 
